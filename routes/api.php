f7380502eafc96736c0cf594bee9140f375fe7f5
Jade Oloroso <100562473+jdojade@users.noreply.github.com>
2023-06-03 16:53:39 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::apiResource('posts', TryController::class);
-Route::get('file-index', [TryController::class, 'index']);
